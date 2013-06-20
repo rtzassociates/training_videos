50065ca2432bdc0a773e36734e8d56c3ad2ed0fd
James Gifford <james@trundle.local>
2013-06-19 21:59:51 -0700

    mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Cachely::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.action_mailer.default_url_options = {host: "training-lacss.getcare.com"}
+  config.time_zone = "Pacific Time (US & Canada)"
 end
